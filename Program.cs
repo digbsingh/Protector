1d94fb905dfb4a88962e458ed71a7e4f9af204ef
digbsingh <digbsingh@gmail.com>
2025-03-03 11:30:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var session = new ProtegrityProtector.Session();
+        Console.WriteLine($"Session Created: {session.SessionId} at {session.CreatedAt}");
+
+        var protector = session.GetProtector();
+        string secret = "HelloWorld";
+        string encrypted = protector.Encrypt(secret);
+        string decrypted = protector.Decrypt(encrypted);
+
+        Console.WriteLine($"Original: {secret}");
+        Console.WriteLine($"Encrypted: {encrypted}");
+        Console.WriteLine($"Decrypted: {decrypted}");
+        Console.WriteLine($"Protector Version: {protector.GetVersion()}");
+
+        string singleProt = protector.Protect("Protegrity1234", "user1", "AlphaNum",Encoding.UTF8.GetBytes("abcd123"), null);
+
+        Console.WriteLine($"protector.Protect: {singleProt}");
+    }
+}
